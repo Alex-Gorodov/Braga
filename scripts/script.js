95d3651a734d6bfb5f30137d47cda6b44ffbcf1d
Alex Gorodov <a.gorodov.22@gmail.com>
2022-05-21 01:10:50 +0300

    lines

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,9 @@
-
 let burger = document.querySelector('.burger-navigation-list');
+let burgerFirstLine = document.querySelector('.burger-btn-line:nth-child(1)');
+let burgerThirdLine = document.querySelector('.burger-btn-line:nth-child(3)');
 
 function burgerOpener() {
   burger.classList.toggle('burger-opened');
+  burgerFirstLine.classList.toggle('open-first-line');
+  burgerThirdLine.classList.toggle('open-third-line');
 }
